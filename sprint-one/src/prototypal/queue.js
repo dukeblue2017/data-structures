7f02c0c0c952c41bad7833d8394976113f42cd08
Hack Reactor Student <student@hackreactor.com>
2017-10-05 11:25:28 -0500

    finished 'queue' for prototypal pattern

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.sizeNum = 0;
+  return someInstance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this [ this.sizeNum ] = value;
+  this.sizeNum++;
+};
 
+queueMethods.dequeue = function() {
+  if (this.sizeNum > 0) {
+    this.sizeNum--;
+    var dequeued = this[0];
+    for (var i = 0; i < this.sizeNum; i++) {
+      this[i] = this[ i + 1 ];
+    }
+    delete this[this.sizeNum + 1];
+    return dequeued;
+
+  }
+};
+
+queueMethods.size = function() {
+  return this.sizeNum;
+};
