1a283243efa37d21cec2805867e7ac1145703163
Hack Reactor Student <student@hackreactor.com>
2017-10-07 16:01:16 -0500

    finished 'hashTable'

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,20 +7,50 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  if (bucket) {
+    for (var i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === k) {
+        bucket[i][1] = v;
+        this._storage.set(index, bucket);
+      }
+    }
+    bucket.push( [k, v] );
+    this._storage.set(index, bucket);
+
+  } else {
+    this._storage.set(index, [ [k, v] ]);
+    
+  }
+
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  if (bucket) {
+    for (var i = 0; i < bucket.length; i++) {
+      var tuple = bucket[i];
+      if (tuple[0] === k) {
+        return tuple[1];
+      }
+    }
+  }
+  return undefined;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, undefined);
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ insert: O(L)?
+ retrieve: O(L)?
+ remove: O(1)?
  */
 
 
