1f539151bca805719b9411717f03628fbb8e4ec0
Hack Reactor Student <student@hackreactor.com>
2017-10-07 16:36:11 -0500

    first test passes on 'BinarySearchTree'

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,20 @@
 var BinarySearchTree = function(value) {
+  this._storage = {};
 };
 
+BinarySearchTree.prototype.insert = function() {
+  return 5;
+};
+
+
+BinarySearchTree.prototype.contains = function() {
+
+};
+
+
+BinarySearchTree.prototype.depthFirstLog = function() {
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
