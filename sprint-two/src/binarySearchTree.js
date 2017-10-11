119b76e70ab0643eaea5be74b88e5f20dbf1878f
Carter Duncan <dukeblue2017@gmail.com>
2017-10-11 11:30:33 -0500

    got first two tests to pass for binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,29 +1,45 @@
 var BinarySearchTree = function(value) {
-  this._storage = {};
+  var newTree = Object.create(BinarySearchTree.prototype)
+  newTree.value = value;
+  newTree.left = null;
+  newTree.right = null;
+  return newTree;
 };
 
 
-BinarySearchTree.prototype.left = function() {
+
+BinarySearchTree.prototype.insert = function(value) {
+	var node = BinarySearchTree(value);
+
+
+	function insertNode(tree) {
+		if (value > tree.value && tree.right === null) {
+		  tree.right = node;
+		} else if (value > tree.value) {
+			insertNode(tree.right);
+	  }
+
+	  if (value < tree.value && tree.left === null) {
+		  tree.left = node;
+		} else if (value < tree.value) {
+			insertNode(tree.left);
+	  }
+
+	}
+
+	insertNode(this);
+	
+};
+
+
+BinarySearchTree.prototype.contains = function(target) {
+
+	
 
 };
 
 
-BinarySearchTree.prototype.right = function() {
-
-};
-
-
-BinarySearchTree.prototype.insert = function() {
-
-};
-
-
-BinarySearchTree.prototype.contains = function() {
-
-};
-
-
-BinarySearchTree.prototype.depthFirstLog = function() {
+BinarySearchTree.prototype.depthFirstLog = function(callback) {
 
 };
 
